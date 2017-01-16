becadb895e3ede419ca5550dea1bb7a647904238
vagrant <vagrant@vagrant.vm>
2017-01-16 17:57:12 +0000

    Single Number Difference, Bit munipulation

---
+++ SingleNumber.py
@@ -0,0 +1,21 @@
+"""
+136 Single Number
+
+Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+"""
+
+class Solution(object):
+    def singleNumber(self, nums):
+
+        result = 0
+        for i in nums:
+            result = result ^ i
+
+        return result
+
+
+        
